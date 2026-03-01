fc6c911bc936f15e051ae7304f1137967c644d6b
Carolin <carolin.svensson@live.se>
2026-03-01 21:47:51 +0100

    Got paradise planning

--- src/util/spreadsheets.ts
+++ src/util/spreadsheets.ts
@@ -8,8 +8,8 @@ import { directories } from './directories.js';
  */
 export const spreadsheets: Spreadsheet[] = [
   {
-    id: '1mo7myqHry5r_TKvakvIhHbcEAEQpSiNoNQoIS8sMpvM',
-    exclude: ['Editor Read Me', 'Read Me', 'Unused Unique IDs'],
+    id: '1x4f8LN_8MtA3-yETG8tYe8aAw8lGPJlQ7oS0aNqtSMM',
+    exclude: ['Read Me'],
     dir: directories.raw,
   },
 
@@ -17,10 +17,5 @@ export const spreadsheets: Spreadsheet[] = [
     id: '1MMbsvDfu59OY9YBEAfHhFJ6O8vRTllNFgMrX7RBZuyI',
     exclude: ['Readme', 'Changelog'],
     dir: directories.translations,
-  },
-  {
-    id: '1x4f8LN_8MtA3-yETG8tYe8aAw8lGPJlQ7oS0aNqtSMM',
-    exclude: ['Readme'],
-    dir: directories.mine,
-  },
+  }
 ];
